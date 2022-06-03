ef4c1d380638e022089011d193d99c3ca9ce9ae2
hao <im_leihao@163.com>
2022-06-03 10:31:38 +0800

    updata

--- Template/STM32_Template/Hardware/usart/bsp_debug_usart.c
+++ Template/STM32_Template/Hardware/usart/bsp_debug_usart.c
@@ -1,41 +1,110 @@
 #include "bsp_debug_usart.h"
 
-/**
- * @brief  usart_init
- * @param  flck 时钟频率
- * 		   usart1频率为72M，其余串口为36M
- * @return 无
- * @note   初始化usart
- */
-void usart_init(uint8_t flck) {
-	float temp;
-	uint16_t mantissa;  //波特率整数
-	uint16_t fraction;  //波特率小数
+ /**
+  * @brief  USART GPIO 配置,工作参数配置
+  * @param  无
+  * @retval 无
+  */
+void USART_Config(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
 
-	temp = (float)(flck*1000000)/(115200*16);  //得到 USARTDIV
-	mantissa = temp;                           //得到整数部分
-	fraction = (temp - mantissa)*16;           //得到小数部分
-	mantissa <<= 4;
-	mantissa += fraction;                      //写入到BRR的最终值
-
-	//打开串口时钟
-	RCC->APB2ENR |= (1 << 14);
-	//打开GPIOA时钟
-	RCC->APB2ENR |= (1 << 2);
+	// 打开串口GPIO的时钟
+	DEBUG_USART_GPIO_APBxClkCmd(DEBUG_USART_GPIO_CLK, ENABLE);
 	
-	//配置TX、RX引脚
-	GPIOA->CRH &= 0xFFFFF00F;  //清空 PIN10 PIN9
-	// TX：复用推挽输出 RX：输入模式
-	GPIOA->CRH |= 0x000008B0;  
+	// 打开串口外设的时钟
+	DEBUG_USART_APBxClkCmd(DEBUG_USART_CLK, ENABLE);
 
-	//复位串口
-	RCC->APB2RSTR |= (1 << 14);
-	//停止复位
-	RCC->APB2RSTR &= ~(1 << 14);
-	//usart工作模式
-	USART1->BRR = mantissa; //波特率
-	USART1->CR1 |= 0x200C;  //使能usart1、无校验位
-	USART1->CR2 &= ~(3 << 12); //1个停止位
+	// 将USART Tx的GPIO配置为推挽复用模式
+	GPIO_InitStructure.GPIO_Pin = DEBUG_USART_TX_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(DEBUG_USART_TX_GPIO_PORT, &GPIO_InitStructure);
+
+  // 将USART Rx的GPIO配置为浮空输入模式
+	GPIO_InitStructure.GPIO_Pin = DEBUG_USART_RX_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(DEBUG_USART_RX_GPIO_PORT, &GPIO_InitStructure);
+	
+	// 配置串口的工作参数
+	// 配置波特率
+	USART_InitStructure.USART_BaudRate = DEBUG_USART_BAUDRATE;
+	// 配置 针数据字长
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	// 配置停止位
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	// 配置校验位
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	// 配置硬件流控制
+	USART_InitStructure.USART_HardwareFlowControl = 
+	USART_HardwareFlowControl_None;
+	// 配置工作模式，收发一起
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	// 完成串口的初始化配置
+	USART_Init(DEBUG_USARTx, &USART_InitStructure);	
+	
+	// 使能串口
+	USART_Cmd(DEBUG_USARTx, ENABLE);	    
+}
+
+/*****************  发送一个字节 **********************/
+void Usart_SendByte( USART_TypeDef * pUSARTx, uint8_t ch)
+{
+	/* 发送一个字节数据到USART */
+	USART_SendData(pUSARTx,ch);
+		
+	/* 等待发送数据寄存器为空 */
+	while (USART_GetFlagStatus(pUSARTx, USART_FLAG_TXE) == RESET);	
+}
+
+/****************** 发送8位的数组 ************************/
+void Usart_SendArray( USART_TypeDef * pUSARTx, uint8_t *array, uint16_t num)
+{
+  uint8_t i;
+	
+	for(i=0; i<num; i++)
+  {
+	    /* 发送一个字节数据到USART */
+	    Usart_SendByte(pUSARTx,array[i]);	
+  
+  }
+	/* 等待发送完成 */
+	while(USART_GetFlagStatus(pUSARTx,USART_FLAG_TC)==RESET);
+}
+
+/*****************  发送字符串 **********************/
+void Usart_SendString( USART_TypeDef * pUSARTx, char *str)
+{
+	unsigned int k=0;
+  do 
+  {
+      Usart_SendByte( pUSARTx, *(str + k) );
+      k++;
+  } while(*(str + k)!='\0');
+  
+  /* 等待发送完成 */
+  while(USART_GetFlagStatus(pUSARTx,USART_FLAG_TC)==RESET)
+  {}
+}
+
+/*****************  发送一个16位数 **********************/
+void Usart_SendHalfWord( USART_TypeDef * pUSARTx, uint16_t ch)
+{
+	uint8_t temp_h, temp_l;
+	
+	/* 取出高八位 */
+	temp_h = (ch&0XFF00)>>8;
+	/* 取出低八位 */
+	temp_l = ch&0XFF;
+	
+	/* 发送高八位 */
+	USART_SendData(pUSARTx,temp_h);	
+	while (USART_GetFlagStatus(pUSARTx, USART_FLAG_TXE) == RESET);
+	
+	/* 发送低八位 */
+	USART_SendData(pUSARTx,temp_l);	
+	while (USART_GetFlagStatus(pUSARTx, USART_FLAG_TXE) == RESET);	
 }
 
 //重定向c库函数printf到串口，重定向后可使用printf函数
